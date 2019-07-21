acc7134e69dc419f3d53802a2b2a17beddc82ecd
Steven <steven.tjhang@gmail.com>
2019-07-21 12:27:56 +0700

    Updates

---
+++ precache-manifest.034971fd6937a151a9d38598ff0fadd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "813434f2cc77cc579eb69580f038fd5d",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "37c66a177210a3140542",
+    "url": "/monster-rolodex/static/css/main.cd3a97ec.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "37c66a177210a3140542",
+    "url": "/monster-rolodex/static/js/main.41005fed.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
